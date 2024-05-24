fc74be4011a947799291a1952c4684a6c3e8db0d
kashifshah2023 <kashif.hussain@digitalcustodian.co>
2024-05-24 23:19:50 +0500

    fix verifytoken

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -1,15 +1,19 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.access_token;
-  if (!token)
-    return res.status(401).json("You are not authorized to access this");
+  try {
+    const token = req.cookies?.access_token;
+    if (!token) {
+      res.status(401).json("You are not authorized to access this");
+    }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json("Your token is invalid");
-    req.user = user;
-    next();
-  });
-  res.status(401).json("You are not authorized to access this. Inalid Token");
-  return;
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) return res.status(403).json("Your token is invalid");
+      req.user = user;
+      next();
+    });
+
+  } catch (error) {
+    res.status(401).json("Inalid Token");
+  }
 };
